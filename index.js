a043583e33217221a9df67d0dd8d5e74adf15eb3
Dana Franklin <danaf@mediaartslab.com>
2014-11-19 16:08:40 -0800

    Continuing to try stuff until something works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
 
   included: function(app, parentAddon) {
     var target = (parentAddon || app);
+    app.import('vendor/ella-star-rating/base.css');
     // Now you can modify the app / parentAddon. For example, if you wanted
     // to include a custom preprocessor, you could add it to the target's
     // registry:
